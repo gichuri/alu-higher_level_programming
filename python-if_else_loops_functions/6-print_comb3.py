770dba6b07e0ee113db324171ee85d8a4e485f36
gichuri <m.ndungu@alustudent.com>
2022-07-04 04:11:11 -0700

    task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(1, 10):
-    for j in range(i+1, 10):
-        if i==8 and j== 9:
-            print('89')
-            break
-        print("{}{} ".format(i,j), end='')
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i < 8:
+            print("{:d}{:d}".format(i, j), end=", ")
+        else:
+            print("{:d}{:d}".format(i, j))
