52df0372d8616d0b2d04ba00fb0e6324d3800f04
gichuri <m.ndungu@alustudent.com>
2022-07-05 07:34:48 -0700

    task 12 correction

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,10 @@ def fizzbuzz():
     numbers = list(range(1, 101))
     for i in numbers:
         if i % 15 == 0 :
-            print(i, 'FizzBuzz', end=' ')
+            print('FizzBuzz', end=' ')
         elif i % 3 == 0 :
-            print (i ,'Fizz', end=' ')
+            print ('Fizz', end=' ')
         elif i % 5 == 0 :
-            print(i , 'Buzz', end=' ')
+            print('Buzz', end=' ')
         else:
             print(i , end=' ')
