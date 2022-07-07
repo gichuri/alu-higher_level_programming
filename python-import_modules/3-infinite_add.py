33b8805fd432baddcdfbb33e94f761e48f1f0783
gichuri <m.ndungu@alustudent.com>
2022-07-07 02:00:23 -0700

    task 3: addition of arguments

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys 
+
+
+def main():
+    args = sys.argv[1:]
+    args_n = int(sys.argv[1:])
+    sum = 0 
+    for i in args_n:
+        sum += args_n 
+    print(sum) 
+
+if __name == __'main'__:
+    main()
+
