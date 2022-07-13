937d55a62ac6c058bea44bc45ebe4c8a08ba2bee
gichuri <m.ndungu@alustudent.com>
2022-07-13 07:58:32 -0700

    reversing numbers trial

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    reverse_list = my_list.reverse()
-    for i in reverse_list:
-        print('{:d}'.format(i))
+    def reverse(my_list):
+        my_list.reverse()
+        return my_list
+    for i in reverse(my_list):
+        print ('{:d}'.format(i))
