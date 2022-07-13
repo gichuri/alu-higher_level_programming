53faa89e6aa74625fc20b47b1a629f716cc3ded8
gichuri <m.ndungu@alustudent.com>
2022-07-13 08:33:08 -0700

    task 4 trials

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,11 +2,8 @@
 def new_in_list(my_list, idx, element):
     original = tuple (my_list)
     if idx < 0 or idx >= len(my_list):
-        print(original)
+        return(original)
     else:
         my_list[idx] = element
         new_list = my_list
         return new_list
-        return original
-        
-
