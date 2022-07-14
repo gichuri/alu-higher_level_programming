a9af369e8402e33f4a632b87a5032083be4b8f10
gichuri <m.ndungu@alustudent.com>
2022-07-14 00:20:42 -0700

    correction of checker 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    def reverse(my_list):
-        my_list.reverse()
-        return my_list
-    for i in reverse(my_list):
-        print('{:d}'.format(i))
+    if my_list == None:
+        return 
+    else:    
+        def reverse(my_list):
+            my_list.reverse()
+            return my_list
+        for i in reverse(my_list):
+            print('{:d}'.format(i))
