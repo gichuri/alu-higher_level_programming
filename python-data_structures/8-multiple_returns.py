06982d56d159408854a94193394c8aec4a670061
gichuri <m.ndungu@alustudent.com>
2022-07-13 23:09:51 -0700

    correct checker

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-     l = len(sentence)
-     if sentence == "":
-         print(0,None)
-     else:
-         return l, sentence[0]
+    l = len(sentence)
+    if sentence == "":
+        return 0,None
+    else:
+        return l, sentence[0]
