c94c91db90bd68eed6b947ba29089fc2d8cc3d42
gichuri <m.ndungu@alustudent.com>
2022-07-15 06:55:21 -0700

    simple deletion

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,8 +2,6 @@
 
 
 def simple_delete(a_dictionary, key=""):
-    if key not in a_dictionary:
-        return 'xx'
-    else:
+    if key in a_dictionary:
         del a_dictionary[key]
-        return (a_dictionary)
+    return a_dictionary
