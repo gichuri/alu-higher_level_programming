14cba7ecc1e6cbb05fec07c22fe1700e1c3a5688
gichuri <m.ndungu@alustudent.com>
2022-07-18 01:45:55 -0700

    tprintining a safe list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+try:
+    def safe_print_list(my_list=[], x=0):
+        my_list2 = my_list[:x]
+        count = 0
+        for i in my_list2:
+            count = count+1
+        print(count)
+except:
+    print("Out of range")
---
+++ python-exceptions/README.md
@@ -0,0 +1,1 @@
+This directory contains codes on python exceptions and errors
