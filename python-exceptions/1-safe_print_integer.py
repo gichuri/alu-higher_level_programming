f29a4951fc3709b02fe6c38ead8795cffa915bff
gichuri <m.ndungu@alustudent.com>
2022-07-18 02:30:51 -0700

    value error

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print('{:d}'.format(value))
         print('True')
-     except(ValueError,TypeError):
+    except(ValueError,TypeError):
         print('False')
