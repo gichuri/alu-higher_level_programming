8bbe40cc2c12d1918f76b4a2ce8ae7a991cb7dff
gichuri <m.ndungu@alustudent.com>
2022-07-18 02:09:26 -0700

    correct checker

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-try:
-    def safe_print_list(my_list=[], x=0):
-        my_list2 = my_list[:x]
-        count = 0
-        for i in my_list2:
-            count = count+1
-        print(''.join(map(str, my_list2)))
-        print(count)
- 
-except:
-    print("Out of range")
+def safe_print_list(my_list=[], x=0):
+    idex = 0
+    while idex < x:
+        try:
+            print("{}".format(my_list[idex]), end='')
+        except IndexError:
+            break
+        idex += 1
+    print()
+    return idex
