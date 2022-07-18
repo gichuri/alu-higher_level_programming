32b93f65173aab37ae7e67e71dff1968d162903a
gichuri <m.ndungu@alustudent.com>
2022-07-18 05:16:55 -0700

    checker correction

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,4 +6,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
        c = None
     finally:
-        print('Inside result : {}'.format(c)
+        print('Inside result : {}'.format(c))
