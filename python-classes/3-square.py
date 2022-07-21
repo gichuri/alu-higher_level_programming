2d902880a6ebb52c2afaa84a6a96f49e53d51600
gichuri <m.ndungu@alustudent.com>
2022-07-21 07:25:16 -0700

    square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+'''
+class square
+defines a square
+'''
+
+
+class Square:
+
+    '''
+    class square
+    Args: side of the square
+    '''
+    def __init__(self, size=0):
+        '''
+
+        :param self:
+        :param __size:
+        :return:sides of square (__size)
+        '''
+        if (type(size)) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        '''multiply __size by itself to get square area'''
+        a= self.__size * self.__size
+        return a
