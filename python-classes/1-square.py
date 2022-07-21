45338fa251d3a6c8d5b56de255337489ed6b5e29
gichuri <m.ndungu@alustudent.com>
2022-07-21 00:17:25 -0700

    initializing square

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+class Square:
+    '''
+    class square
+    Args: side of the square
+    '''
+    def __init__(self, __size):
+        '''
+
+        :param self:
+        :param __size:
+        :return:sides of square (__size)
+        '''
+        self.__size = __size
