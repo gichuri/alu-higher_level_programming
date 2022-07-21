982a6d4dab6d374ee7346431969ab32eafa0f23a
gichuri <m.ndungu@alustudent.com>
2022-07-21 09:59:14 -0700

    prinnting the square

---
+++ python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Module 4-square.py
+defines class Square
+"""
+
+
+class Square:
+    """class Square definition
+    Args: size(int) - side of the square
+    """
+
+    def __init__(self, size=0):
+        """
+                Initialization of square
+                Attributes: Size(int) - defaults to zero if no value
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ setter
+            sets size to value if value is int and greater than 0
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates area of the square"""
+        return (self.__size)**2
+
+    def my_print(self):
+        """printing the square in stdout"""
+        for i in range(1,self.__size+1):
+            for j in range(1,self.__size+1):
+                print('#', end ='')
+            print()
