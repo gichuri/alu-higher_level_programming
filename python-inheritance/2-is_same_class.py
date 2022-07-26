a8a56d280f516e646f78b1422d181631ed03a9e4
gichuri <m.ndungu@alustudent.com>
2022-07-26 03:09:52 -0700

    tatask 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""class to inherit from list"""
+class MyList (list):
+    """mylist is a subclass of list"""
+
+    def print_sorted(self):
+
+        '''method to print the list from which this class inherits'''
+
+        print(sorted(list))
