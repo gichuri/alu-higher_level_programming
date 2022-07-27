0dcd4204edafbc052454248fa0158df7e4e793ab
gichuri <m.ndungu@alustudent.com>
2022-07-27 02:21:57 -0700

    remove isintance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
 
-'''verifying if object belings 
+'''verifying if object belings
     to a specified class'''
 
-def is_same_class(obj, a_class):
-    '''calling builtin magic method isintance'''
 
-    return isinstance(obj, a_class)
+def is_same_class(obj, a_class):
+    '''use builtin type to check obj instance'''
+
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
