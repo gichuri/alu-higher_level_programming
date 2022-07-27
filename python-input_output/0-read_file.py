cfaf23abd68b7f2b7f6877a94d3b19b35911732f
gichuri <m.ndungu@alustudent.com>
2022-07-27 08:19:44 -0700

    try lines

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,7 @@ function to read a file and print content to stdout
 
 
 def read_file(filename=""):
-    '''use with to open and close file''' 
+    '''use with to open and close file'''
 
     with open(filename, encoding="utf-8") as myfile:
-        print(myfile.read())
+        print(myfile.readline())
