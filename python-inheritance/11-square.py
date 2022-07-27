3d5acdd085c5e515728cd18ebe5539cfcd98af51
gichuri <m.ndungu@alustudent.com>
2022-07-27 05:28:03 -0700

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''new class square that extends the super class'''
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+class Square(Rectangle):
+    '''instatiation of new class square'''
+
+    def __init__(self, size):
+        self.__size = size
+        super().integer_validator('size',size)
+
+    def area(self):
+        """override method in super()"""
+
+        return self.__size * self.__size
+
+    def __str__(self):
+
+        '''return a specified string in print()'''
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
