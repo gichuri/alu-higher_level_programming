025eef7c9436aed16769594ba07195a9119f64b2
gichuri <m.ndungu@alustudent.com>
2022-07-27 02:59:20 -0700

    exceptional method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''adding to geometry class'''
+
+
+class BaseGeometry:
+    '''new class as super class'''
+
+    def area(self):
+
+        '''method raise an exemption
+                lack of implementation'''
+
+        raise Exception("area() is not implemented")
