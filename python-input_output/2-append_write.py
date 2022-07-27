0689a1475953ac09ae9c6ac85372a98ff4444bcb
gichuri <m.ndungu@alustudent.com>
2022-07-27 10:30:43 -0700

    append

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 
-"""
-append string at the end of a file
-"""
+"""append string at the end of a file"""
 
 
 def append_write(filename="", text=""):
 
     '''
-
-
-    :param filename:
-    :param text:
+    :param filename
+    :param text
     :return: characters added
     '''
     with open(filename, mode='a', encoding='utf-8') as f:
