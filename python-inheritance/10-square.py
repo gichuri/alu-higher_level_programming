dd4a0ddcb839c006f813ab7298f3ca3ea6845b67
gichuri <m.ndungu@alustudent.com>
2022-07-27 05:15:23 -0700

    task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
 
     def __init__(self, size):
         self.__size = size
-        super().integer_validator('size',size)
+        super().integer_validator('size', size)
 
     def area(self):
         """override method in super()"""
