ff05ef750d868caa01dd56864de7bb489582835e
gichuri <m.ndungu@alustudent.com>
2022-07-27 02:26:50 -0700

    use isintance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+'''verify if obj belongs
+    to class or subclass'''
+
+
+def is_kind_of_class(obj, a_class):
+
+    '''use builtin isintance'''
+
+    return isinstance(obj, a_class)
