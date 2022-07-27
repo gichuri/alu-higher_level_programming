75be14b3adf3543e6ff707f2defc8640e469a7e9
gichuri <m.ndungu@alustudent.com>
2022-07-27 08:50:49 -0700

    write to file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+create new file and return characters written
+"""
+
+
+def write_file(filename="", text=""):
+    '''
+    open file or create new file
+    '''
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(text)
+        print(len(text))
