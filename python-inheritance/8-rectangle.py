8a425213b7d9480a98434204569d2a69d172f976
gichuri <m.ndungu@alustudent.com>
2022-07-27 03:49:18 -0700

    task 8 new trial

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''new class Rectangle extending geometry'''
+
+
+class Rectangle(BaseGeometry):
+        '''instatiating Rectangle object'''
+
+        def __init__(self, width, height):
+            self.__width = width
+            self.__height = height
+            super().integer_validator('width', self.__width)
+            super().integer_validator('height', self.__height)
