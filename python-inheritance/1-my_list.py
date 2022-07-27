9af43e87044508033ae21cf96264627f309fe7e1
gichuri <m.ndungu@alustudent.com>
2022-07-27 05:44:28 -0700

    task one trial

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''new subclass mylist 
+    to extend builtin list'''
+
+
+class Mylist(list):
+
+    '''method to sort list'''
+
+    def print_sorted(self):
+        print(sorted(list))
