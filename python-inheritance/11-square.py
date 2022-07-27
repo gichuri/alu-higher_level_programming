ad7d69ed77573e88aa8f0e133f33105a5ccd6c88
gichuri <m.ndungu@alustudent.com>
2022-07-27 05:29:50 -0700

    task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 '''new class square that extends the super class'''
-Rectangle = __import__('9-rectangle.py').Rectangle
+
+Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
     '''instatiation of new class square'''
