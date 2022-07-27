83f02dcd08eb7f4a391c6ee7ee41bf46dac8ee99
gichuri <m.ndungu@alustudent.com>
2022-07-27 04:17:04 -0700

    nadd str

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+'''new class recangle that extends the class geometry'''
+
+
+class Rectangle(BaseGeometry):
+        '''instatiating Rectangle object'''
+
+        def __init__(self, width, height):
+            self.__width = width
+            self.__height = height
+            super().integer_validator('width', self.__width)
+            super().integer_validator('height', self.__height)
+
+        def area(self):
+            '''this should override the area method in super()'''
+
+            return self.__width * self.__height
+
+        def __str__(self):
+            ''' return a specified str'''
+
+            return "[Rectangle] {}/{}".format(self.__width, self.__height)
