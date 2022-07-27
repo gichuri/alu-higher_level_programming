d4737b88036194e8928e80e97a341ebea9849a37
gichuri <m.ndungu@alustudent.com>
2022-07-27 05:14:06 -0700

    task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
 """new class square that extends the super class"""
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(Rectangle,BaseGeometry):
+class Square(Rectangle):
     '''instatiation of new class square'''
 
     def __init__(self, size):
         self.__size = size
-        BaseGeometry.integer_validator(self, 'size', size)
+        super().integer_validator('size',size)
 
     def area(self):
         """override method in super()"""
