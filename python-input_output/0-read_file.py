5b2033a6c30e56f9dc31e180a5f559bfaef8c095
gichuri <m.ndungu@alustudent.com>
2022-07-27 08:09:40 -0700

    correct checker

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+function to read a file and print content to stdout
+"""
+
 
 def read_file(filename=""):
+    '''use with to open and close file''' 
+
     with open(filename, encoding="utf-8") as myfile:
         print(myfile.read())
