f6011304ad196460bd3e5e7bbe6c3fc54d4bb25b
gichuri <m.ndungu@alustudent.com>
2022-07-28 09:16:18 -0700

    json dict

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """convert class to json"""
-import json
 
 
 def class_to_json(obj):
     """return dict description of obj serialisation"""
-    return json.load(obj)
+    return obj.__dict__
