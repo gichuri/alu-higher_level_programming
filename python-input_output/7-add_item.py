1e9a72b7e84fcdb1f38858363e8a242ad05de84a
gichuri <m.ndungu@alustudent.com>
2022-07-28 08:52:21 -0700

    loadd + add

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+
+all_list = [arg for arg in sys.argv[1:]]
+try:
+    items = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    items = []
+items.extend(all_list)
+save_to_json_file(items, "add_item.json")
