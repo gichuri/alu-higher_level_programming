793887177b7c01d142bf413ab52c789db31d0f8c
gichuri <m.ndungu@alustudent.com>
2022-08-01 09:50:47 +0000

    fetch header

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python3
+
+
+"""display the x-Request-ID variale"""
+
+import sys
+
+
+def fetcher():
+    """fetcher"""
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        header = response.info()
+        print(header["X-Request-Id"])
+
+if __name__ == "__main__":
+    fetcher()
