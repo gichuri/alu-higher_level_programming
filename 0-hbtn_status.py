6d6dcb6841f983bbbc30b5775f277d574d91421a
gichuri <m.ndungu@alustudent.com>
2022-08-02 12:43:30 +0300

    clean up

--- 0-hbtn_status.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""module to fetch a url"""
-
-import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-	html = response.read()
--- 0-rectangle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" defines class rectangle """
-
-
-class Rectangle:
-    """ empty rectangle """
-    pass
