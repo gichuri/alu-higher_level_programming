6e77ee073ebcf8b73720625256049976cc05a09c
gichuri <m.ndungu@alustudent.com>
2022-08-04 20:43:45 +0300

    create and add priviledges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+--creating user and giving them all access 
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
