c865eeacb7fdca191ce3117836a2bb97e592b791
gichuri <m.ndungu@alustudent.com>
2022-08-04 20:38:06 +0300

    show priviledges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- showing all the priviledges of 2 users
-SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
-SHOW GRANTS FOR 'user_0d_2' @ 'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
