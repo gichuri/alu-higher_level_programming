5b7956fc366399ed5571edf710fd7066bc53eeb1
gichuri <m.ndungu@alustudent.com>
2022-08-05 16:40:07 +0300

    trial

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,9 @@
---unique id 
+--unique id
+DROP DATABASE IF EXISTS hbtn_test_db_5;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+INSERT INTO unique_id (name) VALUES ("Holberton School");
+INSERT INTO unique_id (id, name) VALUES (2, "Holberton");
+INSERT INTO unique_id (id, name) VALUES (3, "School");
+INSERT INTO unique_id (id, name) VALUES (4, "C is fun");
+INSERT INTO unique_id (id, name) VALUES (5, "Python is cool");
 CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
