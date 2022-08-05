61b36d846e0ed3d659eabd3414c1e1377eaeeabf
gichuri <m.ndungu@alustudent.com>
2022-08-05 16:17:59 +0300

    cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- create database with foreign key and primary key 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
-CREATE TABLE IF NOT EXISTS cities(
-	id INT NOT NULL AUTO_INCREMENT,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL, 
-	PRIMARY KEY(id),
-	FOREIGN KEY(state_id), REFERENCES states(id) ); 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+       id INT NOT NULL AUTO_INCREMENT,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY(id),
+       FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id) );
