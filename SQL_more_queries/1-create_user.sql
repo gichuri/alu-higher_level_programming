1de117a54a96c3b5af7584166d216ef24850621a
gichuri <m.ndungu@alustudent.com>
2022-08-05 12:18:17 +0300

    This user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
---creating user and giving them all access 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+--creating a user and granting them all the priviledges 
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; 
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'; 
