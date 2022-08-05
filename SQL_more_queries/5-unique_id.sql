99b2aadeb6ad3b39b64e81bad8d0279af63c4db4
gichuri <m.ndungu@alustudent.com>
2022-08-05 16:22:41 +0300

    qn 5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,2 @@
 --unique id 
-CREATE TABLE IF NOT EXISTS unique_id (
-	id INT DEFAULT 1 UNIQUE,
-	name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
