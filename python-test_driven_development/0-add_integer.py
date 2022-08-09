c1f66a637a8163ff2bc0eb34df778f9d304e1c2b
gichuri <m.ndungu@alustudent.com>
2022-08-09 17:23:28 +0300

    test trials

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module describes a method to add two integers
+"""
+
+
+def add_integer(a, b=98):
+    '''
+    add two integers
+
+    '''
+    if type(a) or type(b) == float:
+        a = int(a)
+        b = int(b)
+    elif type(a) is not int:
+        raise TypeError("{} must be an integer".format(a))
+    elif type(b) is not int:
+        raise TypeError("{} must be an integer".format(b))
+    else:
+        return a+b
---
+++ python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+ This are the first methods developed using a test driven aproach
