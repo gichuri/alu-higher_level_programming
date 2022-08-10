1b4b2d95fd2b443605c14be9649329d638a944d5
gichuri <m.ndungu@alustudent.com>
2022-08-10 13:12:21 +0300

    define functions

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
 import unittest
-import 6-max_integer.py
+max_integer = __import__('6-max_integer').max_integer
 
-class TestName(unittest.TestCase):
 
-   def test_name(self):
-       self.assertEquals(6-max_integer.max_integer(list=[1,2,3,4]), 4)
-       self.assertEquals(6-max_integer.max_integer(list=[11,2,3,4]), 11)
-       self.assertEquals(6-max_integer.max_integer(list=[1,2,78,9]), 78)
-       self.assertEquals(6-max_integer.max_integer(list=[1, -2, 78, 9]), 78)
-       self.assertEquals(6-max_integer.max_integer(list=[-1, -2, -78, -9]), -1)
-       self.assertEquals(6-max_integer.max_integer(list=[78]), 78)
-       self.assertEquals(6-max_integer.max_integer(list=[]), None)
+class TestMaxInteger(unittest.TestCase):
+    """ tests for max_integer
+    """
+
+    def test_max_int_basic(self):
+        """ tests normal list of ints
+        """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_int_empty(self):
+        """ tests if list is empty
+        """
+        self.assertEqual(max_integer([]), None)
+
+    def test_max_int_neg(self):
+        """ tests if list has a negative int
+        """
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_max_int_one(self):
+        """ tests if list has only one item
+        """
+        self.assertEqual(max_integer([1]), 1)
+
+if __name__ == '__main__':
+    unittest.main()
