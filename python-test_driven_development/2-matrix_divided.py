e7dde443ed24ba16db55ba2a7f50d300d1305c47
gichuri <m.ndungu@alustudent.com>
2022-08-14 14:30:33 +0300

    add new

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,26 +10,26 @@ def matrix_divided(matrix, div):
     checks if each list in the matrix are the same size
     checks if "div" is an int/float or is 0
     """
-    mes0 = 'matrix must be a matrix (list of lists) of integers/floats'
-    mes1 = 'Each row of the matrix must have the same size'
-    inner_list = []
-    res_list = []
-    if not isinstance(div, (int,float)):
-        raise TypeError('div must be a number')
-    if div == 0:
-        raise ZeroDivisionError('division by zero')
-    if not isinstance(matrix, list):
-       raise mes0
-    for row in matrix:
-        for i in row:
-            if not isinstance(i, (int, float)):
-                raise TypeError(mes0)
-        if len(row) != len(matrix[0]):
-            raise TypeError(mes1)
-        else:
-            for i in row:
-                inner_list.append(round(i/div, 2))
-                res_list.append(inner_list)
+    mes0 = "matrix must be a matrix (list of lists) of integers/floats"
+    mes1 = "Each row of the matrix must have the same size"
+    res_matrix = []
 
-        return res_list
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    for lists in matrix:
+        if len(lists) != len(matrix[0]):
+            raise TypeError(mes1)
+        inner_list = []
+        for items in lists:
+            if not isinstance(items, (int, float)):
+                raise TypeError(mes0)
+            else:
+                inner_list.append(round(items / div, 2))
+        res_matrix.append(inner_list)
+
+    return res_matrix
 
