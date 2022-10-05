226215f8a5654ab811db82745b514f8b4ba9e6a0
martin <m.ndungu@alustudent.com>
2022-10-05 22:08:50 +0300

    langs

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const cars = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
-for (i = 0; i < cars.length; i++){
+for (i = 0; i < cars.length; i++) {
   let name = '';
   name += cars[i]
   console.log(name);
