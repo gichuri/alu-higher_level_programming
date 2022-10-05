b15283e8329c428a70da08b760fdad1d7aa15cd3
martin <m.ndungu@alustudent.com>
2022-10-05 22:43:00 +0300

    7 city

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,6 @@ const cars = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 for (i = 0; i < cars.length; i++) {
   let name = '';
-  name += cars[i]
+  name += cars[i];
   console.log(name);
 }
