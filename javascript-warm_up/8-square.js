54edb1edb19222749eebc5e0cd02b1119a136977
martin <m.ndungu@alustudent.com>
2022-10-05 23:02:07 +0300

    square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ const args = process.argv;
 if (isNaN(Number(args[2]))) {
     console.log('Missing size');
 } else {
-    for (i = 0; i < num; i++) {
+    for (i = 0; i < Number(args[2]); i++) {
     let row = ''
-      for (j = 0; j < num; j++) {
+      for (j = 0; j < Number(args[2]); j++) {
       row+= 'X';
       }
 console.log(row+'')
