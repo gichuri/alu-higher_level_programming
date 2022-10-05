b2d1313d127cc0ffaafd9f26317d8f16dca370c5
martin <m.ndungu@alustudent.com>
2022-10-05 16:49:35 +0300

    if-else

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const args = process.argv;
 if (args.length === 1) {
   console.log('Argument found');
-} else if (args.length > 3) {
+} else if (args.length > 1) {
   console.log('Arguments found');
 } else {
   console.log('No argument');
