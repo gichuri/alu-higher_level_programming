d0b02461e13b348cc19afad57b0358387f6bacc6
martin <m.ndungu@alustudent.com>
2022-10-05 18:05:36 +0300

    to int

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+#!/usr/bin/node
 const args = process.argv;
 if (Number(args[2]) != NaN) {
   console.log('My number:', Number(args[2]));
-} else if ('My number:', Number(args[2]) === NaN) {
+} else if (isNaN(args[2]) === true) {
   console.log('Not a number');
 } else {
-    console.log('Not a number');
+  console.log('Not a number');
 }
