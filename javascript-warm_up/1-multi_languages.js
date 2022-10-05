e9673fa8955b475f5b6f0b24f9fde7e35f2ff6a4
martin <m.ndungu@alustudent.com>
2022-10-05 15:43:39 +0300

    three languages

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const myVar = 'C is fun'
-const mylike = 'Python is cool'
-const myf = 'JavaScript is amazing'
-console.log(myVar)
-console.log(mylike)
-console.log(myf)
+const myVar = 'C is fun';
+const mylike = 'Python is cool';
+const myf = 'JavaScript is amazing';
+console.log(myVar);
+console.log(mylike);
+console.log(myf);
