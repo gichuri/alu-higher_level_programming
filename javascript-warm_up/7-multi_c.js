f91b20e3710dcd9d4bd3c9719f70f04593a4b01c
martin <m.ndungu@alustudent.com>
2022-10-05 22:42:04 +0300

    7 city

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,9 +4,9 @@ let i = 0;
 if (isNaN(args[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (i = 0; i <= Number(args[2]); i++) {
-  let name = '';
-  name += 'C is fun'
-  console.log(name);
+  for (i = 0; i < Number(args[2]); i++) {
+    let name = '';
+    name += 'C is fun';
+    console.log(name);
   }
 }
