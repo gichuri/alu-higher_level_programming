7bc5f854d16b2ec87fd5cadee70a09910079deb1
gichuri <m.ndungu@alustudent.com>
2022-10-10 15:53:10 +0300

    empty object

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-module.imports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    if (w || h <= 0) {
-      Object.create(null);
-    } else {
-      this.height = h;
-      this.width = w;
-    }
+    if (w <= 0 || h <= 0 || !w || !h) return null;
+    this.width = w;
+    this.height = h;
   }
-};
+}
+module.exports = Rectangle;
