45143096cbfa191e94595856408082dfc838241a
gichuri <m.ndungu@alustudent.com>
2022-10-10 17:58:44 +0300

    square extend

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,34 +1,9 @@
 #!/usr/bin/node
-class Rectangle {
-    constructor (w, h) {
-        if (w <= 0 || h <= 0 || !w || !h) return null;
-        this.width = w;
-        this.height = h;
-    }
-// create an intance method called print()
-    print() {
-// print the rectangle using character x
+const Rectangle = require('./4-rectangle');
 
-        for (let i = 0; i < this.height; i++) {
-            let display = ''
-            for (let j = 0; j < this.width; j++) {
-                display += 'x';
-            }
-            console.log(display)
-        }
-    }
-    double() {
-        // multiply width and height by 2
-        this.height*=2;
-        this.width*=2;
-    }
-    rotate() {
-        [this.width, this.height] = [this.height, this.width];
-    }
-  };
 class Square extends Rectangle {
-    super(w, h)
-    constructor (size, size)
-};
-module.exports = Rectangle
-module.exports = Square
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
