47d27ecdfef20efd53a4989965b749ea76ca44ea
gichuri <m.ndungu@alustudent.com>
2022-10-10 16:45:41 +0300

    print rectangle

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,29 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || !w || !h) return null;
-    this.width = w;
-    this.height = h;
-  }
-
-  // create an intance method called print()
-  print () {
-    // print the rectangle using character x
-
-    for (let i = 0; i < this.height; i++) {
-      let display = '';
-      for (let j = 0; j < this.width; j++) {
-        display += 'x';
-      }
-      console.log(display);
+    constructor (w, h) {
+        if (w <= 0 || h <= 0 || !w || !h) return null;
+        this.width = w;
+        this.height = h;
     }
-  }
-}
-module.exports = Rectangle;
+// create an intance method called print()
+    print() {
+// print the rectangle using character x
+
+        for (let i = 0; i < this.height; i++) {
+            let display = ''
+            for (let j = 0; j < this.width; j++) {
+                display += 'x';
+            }
+            console.log(display)
+        }
+    }
+    double() {
+        // multiply width and height by 2
+        this.height*=2;
+        this.width*=2;
+    }
+    rotate() {
+        [this.width, this.height] = [this.height, this.width];
+    }
+  };
+module.exports = Rectangle
